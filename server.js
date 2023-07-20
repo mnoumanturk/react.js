dab8a0bb1902dacc77af5abaffe95f7077114785
mnoumanturk <127292842+mnoumanturk@users.noreply.github.com>
2023-07-20 15:26:41 +0500

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 
 app.use(express.static(path.join(__dirname, " ")));
-app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
 
 const PORT = process.env.PORT || 3009;
 
